f356e85bb1916b528d7bd023cd16009b1f57ecf2
Akhilesh <akhi.singh1989@gmail.com>
2025-07-03 18:58:59 +1000

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -1,43 +1,35 @@
-# game_logic.py
 import random
 
 BOARD_SIZE = 20
 
-class SnakeGame:
-    def __init__(self, num_players):
-        self.board = [['' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-        self.snakes = {}
-        for i in range(num_players):
-            self.snakes[f'P{i+1}'] = {
-                'body': [(random.randint(0, BOARD_SIZE-1), random.randint(0, BOARD_SIZE-1))],
-                'direction': random.choice(['UP', 'DOWN', 'LEFT', 'RIGHT']),
-                'alive': True,
-                'points': 0
-            }
+class Snake:
+    def __init__(self, name):
+        self.name = name
+        self.body = [(random.randint(0, BOARD_SIZE - 1), random.randint(0, BOARD_SIZE - 1))]
+        self.direction = random.choice(["UP", "DOWN", "LEFT", "RIGHT"])
+        self.alive = True
+        self.score = 0
 
-    def move(self, player_id):
-        snake = self.snakes[player_id]
-        if not snake['alive']: return
-        head_x, head_y = snake['body'][0]
-        dir = snake['direction']
-        new_head = {
-            'UP': (head_x, head_y - 1),
-            'DOWN': (head_x, head_y + 1),
-            'LEFT': (head_x - 1, head_y),
-            'RIGHT': (head_x + 1, head_y)
-        }[dir]
-
-        # Collision check
-        if not (0 <= new_head[0] < BOARD_SIZE and 0 <= new_head[1] < BOARD_SIZE):
-            snake['alive'] = False
+    def move(self):
+        if not self.alive:
             return
+        head_x, head_y = self.body[0]
+        dx, dy = {
+            "UP": (0, -1),
+            "DOWN": (0, 1),
+            "LEFT": (-1, 0),
+            "RIGHT": (1, 0)
+        }[self.direction]
+        new_head = (head_x + dx, head_y + dy)
+        if 0 <= new_head[0] < BOARD_SIZE and 0 <= new_head[1] < BOARD_SIZE:
+            self.body.insert(0, new_head)
+            self.body.pop()
+            self.score += 1
+        else:
+            self.alive = False
 
-        # Update position
-        snake['body'].insert(0, new_head)
-        snake['body'].pop()
-
-    def update_direction(self, player_id, direction):
-        self.snakes[player_id]['direction'] = direction
-
-    def get_game_state(self):
-        return self.snakes
+def get_winner(snakes):
+    alive = [s for s in snakes if s.alive]
+    if len(alive) == 1:
+        return alive[0].name
+    return None
